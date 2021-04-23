0fc52d86aa1fad4c92338e9e15dfd4d9efff425d
ty-peng <tiany.dev@qq.com>
2021-04-23 13:44:38 +0800

    leetcode(15): 三数之和

---
+++ src/com/typeng/algorithm/practice/leetcode/15.三数之和.java
@@ -0,0 +1,91 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=15 lang=java
+ *
+ * [15] 三数之和
+ *
+ * https://leetcode-cn.com/problems/3sum/description/
+ *
+ * algorithms
+ * Medium (31.85%)
+ * Likes:    3266
+ * Dislikes: 0
+ * Total Accepted:    490.3K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '[-1,0,1,2,-1,-4]'
+ *
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0
+ * 且不重复的三元组。
+ * 
+ * 注意：答案中不可以包含重复的三元组。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [0]
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * -10^5 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return Collections.emptyList();
+        }
+        List<List<Integer>> result = new LinkedList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2 && nums[i] <= 0; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int s = nums[i] + nums[l] + nums[r];
+                if (s > 0) {
+                    r--;
+                } else if (s < 0) {
+                    l++;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while(l < r && nums[l] == nums[l + 1]) l++;
+                    while(l <r && nums[r] == nums[r - 1]) r--;
+                    l++;
+                    r--;
+                }
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
+
