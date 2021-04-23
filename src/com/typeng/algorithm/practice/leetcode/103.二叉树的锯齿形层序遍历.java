73b535b1bffe70e6db74897dbc0db703708c4c72
ty-peng <tiany.dev@qq.com>
2021-04-23 14:17:04 +0800

    leetcode(103): 二叉树的锯齿形层序遍历

---
+++ src/com/typeng/algorithm/practice/leetcode/103.二叉树的锯齿形层序遍历.java
@@ -0,0 +1,95 @@
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=103 lang=java
+ *
+ * [103] 二叉树的锯齿形层序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (57.12%)
+ * Likes:    437
+ * Dislikes: 0
+ * Total Accepted:    133.2K
+ * Total Submissions: 233.2K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ * 
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 返回锯齿形层序遍历如下：
+ * 
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [20,9],
+ * ⁠ [15,7]
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean positiveOrder = true;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> currentLevelValList = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                if (positiveOrder) {
+                    currentLevelValList.add(curr.val);
+                } else {
+                    currentLevelValList.add(0, curr.val);
+                }
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+            positiveOrder = !positiveOrder;
+            result.add(currentLevelValList);
+        }
+        return result;
+    }
+}
+// @lc code=end
+
